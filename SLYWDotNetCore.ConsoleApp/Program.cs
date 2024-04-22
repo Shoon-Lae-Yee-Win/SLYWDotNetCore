fa8f974b7e26365a0002a9c5d14df66363517b34
Shoon Lae Yee Win <shoonlaeyeewin1602@gmail.com>
2024-04-22 21:43:29 +0630

    [Complete]Ado.Net CRUD

--- SLYWDotNetCore.ConsoleApp/Program.cs
+++ SLYWDotNetCore.ConsoleApp/Program.cs
@@ -1,3 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Data;
+using System.Data.SqlClient;
+using SLYWDotNetCore.ConsoleApp;
+
 Console.WriteLine("Hello, World!");
+
+AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
+//adoDotNetExample.Read();
+//adoDotNetExample.Create("title", "author", "content");
+//adoDotNetExample.Update(12, "test title", "test author", "test content");
+//adoDotNetExample.Delete(13);
+adoDotNetExample.Edit(13);
+adoDotNetExample.Edit(1);
+
 Console.ReadKey();
