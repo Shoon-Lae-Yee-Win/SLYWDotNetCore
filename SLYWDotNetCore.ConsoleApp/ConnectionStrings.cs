0f217ec92287440beb873aa5e894d640c427bc91
Shoon Lae Yee Win <shoonlaeyeewin1602@gmail.com>
2024-04-25 22:48:03 +0630

    [Complete] Dapper CRUD

---
+++ SLYWDotNetCore.ConsoleApp/ConnectionStrings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SLYWDotNetCore.ConsoleApp
+{
+    internal static class ConnectionStrings
+    {
+        public static SqlConnectionStringBuilder SqlConnectionStringBuilder = new SqlConnectionStringBuilder()
+        {
+            DataSource = ".", // Server Name
+            InitialCatalog = "DotNetTrainingBatch4", // DataBase Name
+            UserID = "sa", //User Name
+            Password = "sa123@", // User Password
+        };
+    }
+}
